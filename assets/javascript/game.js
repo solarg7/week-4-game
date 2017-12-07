55b7f295870048de0b71c30fa1d48bcbcb68b36a
Del Solar <marcelo.delsolar@siemens.com>
2017-12-06 20:16:51 -0500

    HW4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,8 +27,17 @@ window.onload = function() {
 	        imageF0.attr("src", fighterLibrary[i].src);
 	        healthP0.text(fighterLibrary[i].healthPointF)
 
+
+
 	        container11.attr("id", ides);
-	        container11.addClass("container2")
+	        container11.addClass("container2 containerButton")
+	        container11.attr("fight", fighterLibrary[i].name);
+
+	        container11.attr("fightFoto", fighterLibrary[i].src);
+
+			container11.attr("fightHealth", fighterLibrary[i].healthPointF);
+
+
 	           	$("#linea1").append(container11);
 
 	        	$("#" + ides).append(nameF0);
@@ -38,13 +47,66 @@ window.onload = function() {
 	        	$("#" + ides).append(healthP0);
 	        	//$("#nameF").text(nameFighter1);
 
-        
-		
-
-
-		
         }
-	  
+	  $(".containerButton").on("click", function () {
+
+	  	var yourFighter = $("<a>");
+	  	yourFighter.text($(this).attr("fight.name"))
+	  	yourFighter.addClass("container2");
+
+
+		$("#linea1").html(yourFighter);
+	    
+	    $("#" + ides).append(yourFighter);
+
+	    $("#" + ides).append(imageF0);		
+
+
+    		var ides = "cont" + i
+
+	        //hacerlo relativo
+	        var container11 = $("<a>");
+
+	        var nameFighter1 = $("<div>");
+			var healthP0= $("<div>");
+
+	        var nameF0 = $("<div>");
+	        var imageF0 = $("<img>")
+
+	        
+
+			nameF0.text($(this).attr("fight"));
+	        imageF0.src($(this).attr("fightFoto"));
+			healthP0.text($(this).attr("fightHealth"));
+
+
+
+	        container11.attr("id", ides);
+	        container11.addClass("container2 containerButton")
+	           	$("#linea1").html(container11);
+
+	        	$("container2").append(nameF0);
+
+	        	$("#container2").append(imageF0);
+
+	        	$("#container2").append(healthP0);
+	        	//$("#nameF").text(nameFighter1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	  	// body...
+	  })
 
 /*$('#button2').prepend(fighterImage);*/
 
