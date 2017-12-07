fe55faccf3b41e37b961cb38b32d60bdcd5f335b
Del Solar <marcelo.delsolar@siemens.com>
2017-12-06 22:37:36 -0500

    HW4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ window.onload = function() {
 	$(document).ready(function() {
       // Here we are provided an initial array of letters.
       // Use this array to dynamically create buttons on the screen.s
-      var fighterLibrary = [{name: "Boba Fett", src: "./assets/images/bobafett.png", healthPointF: 120}, {name: "Chewbacca", src: "./assets/images/chewbacca.png", healthPointF: 120}, {name: "Han Solo", src: "./assets/images/hansolo.png" , healthPointF: 120}, {name: "Darth Vader", src: "./assets/images/darthvader.png", healthPointF: 150}];
+      var fighterLibrary = [{name: "Boba Fett", src: "./assets/images/bobafett.png", healthPointF: 120, inde: "cont0"}, {name: "Chewbacca", src: "./assets/images/chewbacca.png", healthPointF: 120, inde: "cont1"}, {name: "Han Solo", src: "./assets/images/hansolo.png" , healthPointF: 120, inde: "cont2"}, {name: "Darth Vader", src: "./assets/images/darthvader.png", healthPointF: 150, inde: "cont3"}];
       
       	
       	
@@ -37,6 +37,9 @@ window.onload = function() {
 
 			container11.attr("fightHealth", fighterLibrary[i].healthPointF);
 
+			container11.attr("indexX", fighterLibrary[i].inde);
+			console.log(container11.indexX)
+
 
 	           	$("#linea1").append(container11);
 
@@ -56,12 +59,7 @@ window.onload = function() {
 	  	yourFighter.addClass("container2");
 
 
-		$("#linea1").html(yourFighter);
-	    
-	    $("#" + ides).append(yourFighter);
-
-	    $("#" + ides).append(imageF0);		
-
+		
 
     		var ides = "cont" + i
 
@@ -73,6 +71,7 @@ window.onload = function() {
 	        var nameF0 = $("<div>");
 	        var imageF0 = $("<img>")
 
+
 	        
 
 			nameF0.text($(this).attr("fight"));
@@ -80,9 +79,13 @@ window.onload = function() {
 			healthP0.text($(this).attr("fightHealth"));
 
 
+			var indice = "";
+			indice = ($(this).attr("indexX"));
+
+
 
 	        container11.attr("id", ides);
-	        container11.addClass("container2 containerButton")
+	        container11.addClass("container2 containerButton2")
 	           	$("#linea1").html(container11);
 
 	        	$(".container2").append(nameF0);
@@ -92,43 +95,51 @@ window.onload = function() {
 	        	$(".container2").append(healthP0);
 	        	//$("#nameF").text(nameFighter1);
 
-	        		for (var i = 0; i < fighterLibrary.length; i++) {    
-	        var ides = "cont" + i
-
-	        //hacerlo relativo
-	        var container11 = $("<a>");
-
-	        var nameFighter1 = $("<div>");
-			var healthP0= $("<div>");
-
-	        var nameF0 = $("<div>");
-	        var imageF0 = $("<img>")
-	        var nameF = fighterLibrary[i].name;
-	        nameFighter1.attr("id", nameF);
-	        nameFighter1 = fighterLibrary[i].name
-	        nameF0.text(fighterLibrary[i].name);
-	        imageF0.attr("src", fighterLibrary[i].src);
-	        healthP0.text(fighterLibrary[i].healthPointF)
+	        
 
 
+	        for (var i = 0; i < fighterLibrary.length; i++) {    
+		        var ides = "cont" + i
 
-	        container11.attr("id", ides);
-	        container11.addClass("container2 containerButton")
-	        container11.attr("fight", fighterLibrary[i].name);
+		        //hacerlo relativo
+		        var container11 = $("<a>");
 
-	        container11.attr("fightFoto", fighterLibrary[i].src);
+		        var nameFighter1 = $("<div>");
+				var healthP0= $("<div>");
 
-			container11.attr("fightHealth", fighterLibrary[i].healthPointF);
+		        var nameF0 = $("<div>");
+		        var imageF0 = $("<img>")
+		        var nameF = fighterLibrary[i].name;
+		        nameFighter1.attr("id", nameF);
+		        nameFighter1 = fighterLibrary[i].name
+		        nameF0.text(fighterLibrary[i].name);
+		        imageF0.attr("src", fighterLibrary[i].src);
+		        healthP0.text(fighterLibrary[i].healthPointF)
 
 
-	           	$("#linea2").append(container11);
 
-	        	$("#" + ides).append(nameF0);
+		        container11.attr("id", ides);
+		        container11.addClass("container2 containerButton")
+		        container11.attr("fight", fighterLibrary[i].name);
 
-	        	$("#" + ides).append(imageF0);
+		        container11.attr("fightFoto", fighterLibrary[i].src);
 
-	        	$("#" + ides).append(healthP0);
-	    }
+				container11.attr("fightHealth", fighterLibrary[i].healthPointF);
+
+
+		           	$("#linea2").append(container11);
+
+		        	$("#" + ides).append(nameF0);
+
+		        	$("#" + ides).append(imageF0);
+
+		        	$("#" + ides).append(healthP0);
+	    	}
+	    	
+	    	var idc = "cont"+indice
+	    	console.log(indice)
+	    	$("#"+ indice).hide();
+
 	  })
 
 /*$('#button2').prepend(fighterImage);*/
