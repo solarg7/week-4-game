abbafb66f1136a87940a405f8f77bc71d2ca92f6
Del Solar <marcelo.delsolar@siemens.com>
2017-12-06 23:50:14 -0500

    HW4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -81,6 +81,7 @@ window.onload = function() {
 
 			var indice = "";
 			indice = ($(this).attr("indexX"));
+			console.log(indice)
 
 
 
@@ -119,14 +120,14 @@ window.onload = function() {
 
 
 		        container11.attr("id", ides);
-		        container11.addClass("container2 containerButton")
+		        container11.addClass("container2 containerButton3")
 		        container11.attr("fight", fighterLibrary[i].name);
 
 		        container11.attr("fightFoto", fighterLibrary[i].src);
 
 				container11.attr("fightHealth", fighterLibrary[i].healthPointF);
 
-
+				
 		           	$("#linea2").append(container11);
 
 		        	$("#" + ides).append(nameF0);
@@ -140,8 +141,72 @@ window.onload = function() {
 	    	console.log(indice)
 	    	$("#"+ indice).hide();
 
+
+	    	$(".containerButton3").on("click", function () {
+
+				  	var yourFighter = $("<a>");
+				  	yourFighter.text($(this).attr("fight.name"))
+				  	yourFighter.addClass("container2");
+
+				var indice0 = "";
+				indice0 = ($(this).attr("indexX"));
+				console.log(indice0);
+				$("#"+ indice0).hide();
+		
+
+	    		var ides = "cont" + i
+
+		        //hacerlo relativo
+		        var container11 = $("<a>");
+
+		    	var healthP0= $("<div>");
+
+		        var nameF0 = $("<div>");
+		        var imageF0 = $("<img>")
+
+
+		        
+
+				nameF0.text($(this).attr("fight"));
+		        imageF0.attr("src", ($(this).attr("fightFoto")));
+				healthP0.text($(this).attr("fightHealth"));
+
+
+
+
+
+
+
+
+		        container11.attr("id", ides);
+		        container11.addClass("container3")
+		           	$("#linea3").html(container11);
+
+		        	$(".container3").append(nameF0);
+
+		        	$(".container3").append(imageF0);
+
+		        	$(".container3").append(healthP0);
+		        	//$("#nameF").text(nameFighter1);
+
+		        
+
+
+
+
+		        
+		    	
+
+
+		  })
+
+
 	  })
 
+
+
+
+	  
 /*$('#button2').prepend(fighterImage);*/
 
 	  /*$('#theIm').attr("src", fighterLibrary[2].src);*/
